d81d5bf4526b5a20c6c697f9fdebef098fd78eb7
engida32 <engida32lishan@gmail.com>
2024-05-08 15:41:59 +0300

    configure next config for keys

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: process.env.BASE_URL,
+    API_KEY: process.env.API_KEY,
+    TOKEN: process.env.API_TOKEN,
+  },
+  images: {
+    domains: [],
+  },
+};
 
 export default nextConfig;

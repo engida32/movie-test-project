11deecee918ec8cb79543126818aefb3e72dd740
engida32 <engida32lishan@gmail.com>
2024-05-08 16:45:17 +0300

    implement movie detail page

--- src/app/movies/[movie_id]/page.tsx
+++ src/app/movies/[movie_id]/page.tsx
@@ -1,7 +1,93 @@
+"use client";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const page = () => {
-  return <div>this movie detail</div>;
+const MovieDetail = ({ params }: { params: { movie_id: string } }) => {
+  const router = useRouter();
+  const getMovie = useQuery({
+    queryKey: ["movie", params.movie_id],
+    queryFn: () =>
+      axios.get(`${process.env.API_URL}/movie/${params.movie_id}`, {
+        params: {
+          api_key: process.env.API_KEY,
+        },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+        },
+      }),
+
+    enabled: Boolean(process.env.ACCESS_TOKEN),
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+  });
+
+  React.useEffect(() => {
+    getMovie.refetch();
+  }, [params.movie_id]);
+  return (
+    <div>
+      {getMovie.isLoading && "Loading..."}
+      {getMovie.isError && "Error"}
+      {getMovie.isSuccess && (
+        <Stack
+          sx={{
+            width: "50%",
+            height: "100%",
+            margin: "auto",
+
+            justifyContent: "center",
+            padding: "20px",
+          }}
+        >
+          <Button
+            sx={{
+              backgroundColor: "black",
+              color: "white",
+              width: "fit-content",
+              px: "20px",
+              "&:hover": {
+                backgroundColor: "black",
+                color: "white",
+              },
+            }}
+            onClick={() => router.push("/")}
+          >
+            Go Back
+          </Button>
+          <Typography
+            sx={{
+              color: "black",
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+              textAlign: "center",
+              margin: "20px",
+            }}
+          >
+            {getMovie.data?.data?.title}
+          </Typography>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${getMovie.data?.data?.poster_path}`}
+            alt={getMovie.data?.data?.title}
+            style={{ width: "100%", height: "500px", objectFit: "contain" }}
+          />
+          <p>{getMovie.data?.data?.overview}</p>
+          <Box>
+            <h2>Genres</h2>
+            <Stack>
+              {getMovie.data?.data?.genres.map((genre: any) => (
+                <span key={genre.id}>{genre.name}</span>
+              ))}
+            </Stack>
+          </Box>
+        </Stack>
+      )}
+    </div>
+  );
 };
 
-export default page;
+export default MovieDetail;
